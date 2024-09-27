1ae50052ead515d4129effa7f73ed72f7eb1a292
Dzher <2539925009@qq.com>
2024-09-27 17:02:06 +0800

    add mainpanel dlg which support tray icon

--- src/main.py
+++ src/main.py
@@ -1,19 +1,12 @@
-import keyboard.keycounter as keycounter
+import mainpanel.panelwindow as panel
 
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QDesktopWidget
+from PyQt5.QtWidgets import QApplication
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
 
-    window = QWidget()
-    window.setWindowTitle("Summary")
-    window.setMinimumSize(800, 450)
-    desktop = QDesktopWidget()
-    screen_rect = desktop.screenGeometry(0)
-    window.move((screen_rect.width() - window.width()) // 2,
-                (screen_rect.height() - window.height()) // 2)
-
+    window = panel.PanelWindow()
     window.show()
 
     sys.exit(app.exec_())
---
+++ src/mainpanel/panelwindow.py
@@ -0,0 +1,89 @@
+from PyQt5.QtWidgets import QApplication, QMainWindow, QDesktopWidget, QSystemTrayIcon, QMenu, QMenuBar, QAction, \
+    QMessageBox
+from PyQt5.QtCore import QTimer, QEvent
+
+
+class PanelWindow(QMainWindow):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.key_counter_dlg = None
+        self.tray_icon = None
+        self.init_ui()
+        self.init_menus()
+        self.init_tray_icon()
+
+    def init_ui(self):
+        self.setWindowIcon(QApplication.style().standardIcon(QApplication.style().SP_ComputerIcon))
+        self.setWindowTitle('Summary')
+        self.setMinimumSize(800, 450)
+        desktop = QDesktopWidget()
+        screen_rect = desktop.screenGeometry(0)
+        self.move((screen_rect.width() - self.width()) / 2, (screen_rect.height() - self.height()) / 2)
+
+    def init_menus(self):
+        menu_bar = QMenuBar()
+
+        file_menu = QMenu()
+        quit_action = QAction("Quit")
+        quit_action.triggered.connect(self.close)
+        file_menu.addAction(quit_action)
+
+        start_menu = QMenu("Start")
+        keyboard_action = QAction("KeyCounter")
+        start_menu.addAction(keyboard_action)
+
+        setting_menu = QMenu("Setting")
+        setting_edit_action = QAction("Edit")
+        setting_menu.addAction(setting_edit_action)
+
+        menu_bar.addMenu(file_menu)
+        menu_bar.addMenu(start_menu)
+        menu_bar.addMenu(setting_menu)
+
+        self.setMenuBar(menu_bar)
+
+    def init_tray_icon(self):
+        self.tray_icon = QSystemTrayIcon(self)
+        self.tray_icon.setIcon(QApplication.style().standardIcon(QApplication.style().SP_ComputerIcon))
+
+        tray_icon_menu = QMenu()
+        restore_action = QAction("Restore")
+        restore_action.triggered.connect(self.showNormal)
+        quit_action = QAction("Quit")
+        quit_action.triggered.connect(self.close)
+
+        statistics_menu = QMenu("Statistics")
+        keyboard_action = QAction("KeyCounter")
+        keyboard_action.triggered.connect(self.show_key_counter)
+        statistics_menu.addAction(keyboard_action)
+
+        tray_icon_menu.addAction(restore_action)
+        tray_icon_menu.addSeparator()
+        tray_icon_menu.addMenu(statistics_menu)
+        tray_icon_menu.addSeparator()
+        tray_icon_menu.addAction(quit_action)
+
+        self.tray_icon.setContextMenu(tray_icon_menu)
+        self.tray_icon.setToolTip("Summary")
+        self.tray_icon.show()
+
+    def show_key_counter(self):
+        self.key_counter_dlg.show()
+
+    def changeEvent(self, event):
+        if event.type() == QEvent.WindowStateChange:
+            if self.isMinimized():
+                QTimer.singleShot(0, self.hide)
+                self.tray_icon.showMessage("Minimized", "Summary is now minimized to tray.",
+                                           QSystemTrayIcon.Information, 500)
+
+        super().changeEvent(event)
+
+    def closeEvent(self, event):
+        result = QMessageBox.question(self, "Warning",
+                                      "Are you sure you want exit?\nClick 'Yes' to exit and 'No' to hide.")
+        if QMessageBox.No == result:
+            self.hide()
+            event.ignore()
+        else:
+            super().closeEvent(event)
